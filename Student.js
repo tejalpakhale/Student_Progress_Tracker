e8e3b02f72da8502510af7f59f4fa179e37b10ae
tejalpakhale <137317935+tejalpakhale@users.noreply.github.com>
2025-04-17 20:44:12 +0530

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  marks: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Student', StudentSchema);
